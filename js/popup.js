52af5b84a7eff145901d6325e77c1751397336b0
Merkif <nokiaman1997@gmail.com>
2019-06-11 10:03:23 +0300

    размеры для карточек производителей

---
+++ js/popup.js
@@ -0,0 +1,21 @@
+var addCartBtn = document.querySelectorAll(".buy__btn");
+var popupCart = document.querySelector(".modal-cart");
+var cartClose = popupCart.querySelector(".modal-close");
+for (var i = 0; i < addCartBtn.length; i++) {
+  addCartBtn[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popupCart.classList.add("modal--open");
+  });
+}
+cartClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupCart.classList.remove("modal--open");
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popupCart.classList.contains("modal--open")) {
+      popupCart.classList.remove("modal--open");
+    }
+  }
+});
