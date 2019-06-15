88eba39ed05c81563900ebf509eb9ff75a71280c
Merkif <nokiaman1997@gmail.com>
2019-06-15 21:03:04 +0300

    Финальные изменения перед защитой личного проекта

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 /* modal write us */
 var writeUs = document.querySelector(".js-write__us--btn");
 var popupWriteUs = document.querySelector(".modal-write-us");
@@ -58,21 +57,19 @@ window.addEventListener("keydown", function (evt) {
       popupCart.classList.remove("modal--open");
     }
   }
-});
-
-/*tabs */
-document.addEventListener('DOMContentLoaded', function (e) {
-  'use strict';
-  var list = document.querySelectorAll('.tab-nav a');
+}); /*tabs */
+document.addEventListener("DOMContentLoaded", function (e) {
+  "use strict";
+  var list = document.querySelectorAll(".tab-nav a");
   list = Array.prototype.slice.call(list, 0);
   list.forEach(function (el, i, ar) {
     el.addEventListener("click", function (evt) {
       evt.preventDefault();
-      var tab = document.querySelector(el.getAttribute('href'));
-      document.querySelector('.tab-nav .js-tab--active').classList.remove('js-tab--active');
-      document.querySelector('.tabs-content__list .js-tab--active').classList.remove('js-tab--active');
-      el.classList.add('js-tab--active');
-      tab.classList.add('js-tab--active');
+      var tab = document.querySelector(el.getAttribute("href"));
+      document.querySelector(".tab-nav .js-tab--active").classList.remove("js-tab--active");
+      document.querySelector(".tabs-content__list .js-tab--active").classList.remove("js-tab--active");
+      el.classList.add("js-tab--active");
+      tab.classList.add("js-tab--active");
     })
   })
 });
